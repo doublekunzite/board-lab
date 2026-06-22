507a5bde770d3fd50f7161f4cce4b3bee09763ee
doublekunzite <supporttheresistance@proton.me>
2026-06-21 21:09:19 -0500

    process page

--- public/nav.js
+++ public/nav.js
@@ -57,8 +57,9 @@ if (navScriptTag) {
         { name: 'Home', href: `${rootPath}index.html` },
         { name: 'Board Map', href: `${rootPath}board-layout.html` },
         { name: 'AI Setter', href: `${rootPath}ai-setter.html` },
-        { name: 'Moves', href: `${rootPath}#moves` } // Adjust if your moves index has a different name
-    ];
+        { name: 'Moves', href: `${rootPath}#moves` }, // Adjust if your moves index has a different name
+		{ name: 'Process', href: `${rootPath}process.html` }
+	];
 
     let html = '';
     links.forEach(link => {
